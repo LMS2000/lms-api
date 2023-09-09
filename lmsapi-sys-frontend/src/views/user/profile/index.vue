8c3c25e26863d4b6d777fa17157e82514931fea1
LMS <956287248@qq.com>
2023-09-09 19:19:53 +0800

    优化前端

--- lmsapi-sys-frontend/src/views/user/profile/index.vue
+++ lmsapi-sys-frontend/src/views/user/profile/index.vue
@@ -12,10 +12,10 @@
             </div>
             <ul class="list-group list-group-striped">
               <li class="list-group-item">
-                <svg-icon icon-class="user" />用户名称
+                <svg-icon icon-class="user" />账号
                 <div class="pull-right">{{ user.username }}</div>
               </li>
-             <li class="list-group-item">
+              <li class="list-group-item">
                 <svg-icon icon-class="user" />用户昵称
                 <div class="pull-right">{{ user.nickname }}</div>
               </li>
@@ -23,10 +23,7 @@
                 <svg-icon icon-class="email" />用户邮箱
                 <div class="pull-right">{{ user.email }}</div>
               </li>
-           <!--   <li class="list-group-item">
-                <svg-icon icon-class="peoples" />所属角色
-                <div class="pull-right">{{ roleGroup }}</div>
-              </li> -->
+
               <li class="list-group-item">
                 <svg-icon icon-class="date" />创建日期
                 <div class="pull-right">{{ user.createTime }}</div>
@@ -45,12 +42,17 @@
               <userInfo :user="user" />
             </el-tab-pane>
             <el-tab-pane label="修改密码" name="resetPwd">
-              <resetPwd />
+              <resetPwd :user="user" />
             </el-tab-pane>
           </el-tabs>
         </el-card>
       </el-col>
     </el-row>
+
+
+
+
+
   </div>
 </template>
 
@@ -58,12 +60,34 @@
 import userAvatar from "./userAvatar";
 import userInfo from "./userInfo";
 import resetPwd from "./resetPwd";
-import { getInfo } from "../../../api/user.js";
+import { getInfo,sendEmail } from "../../../api/user.js";
 
 export default {
   name: "profile",
   components: { userAvatar, userInfo, resetPwd },
   data() {
+    const validateCode = (rule, value, callback) => {
+    	if (value.length < 5||value.length>5) {
+    		callback(new Error('请输入5位的校验码'))
+    	} else {
+    		callback()
+    	}
+    }
+
+    const validateEmail = (rule, value, callback) => {
+    	if (value.length < 5) {
+    		callback(new Error('请输入正确的email'))
+    	} else {
+    		callback()
+    	}
+    }
+    const validateEmailCode = (rule, value, callback) => {
+    	if (value.length < 5) {
+    		callback(new Error('请输入正确的邮箱验证码'))
+    	} else {
+    		callback()
+    	}
+    }
     return {
       user: {},
       roleGroup: {},
@@ -85,3 +109,20 @@ export default {
   }
 };
 </script>
+<style scoped>
+
+
+  .check-code-panel {
+  	width: 100%;
+  	display: flex;
+
+  	.check-code {
+  		margin-left: 5px;
+  		cursor: pointer;
+  	}
+  }
+  .check-code-panel .el-input{
+    width: 250x;
+  }
+
+</style>
